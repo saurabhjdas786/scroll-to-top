2cc703d2058df89e0c612b672421338a33710a2b
saurabhjdas786 <saurabhjdas786@gmail.com>
2021-05-11 01:45:26 +0530

    feat(usability): add button for scrolling to the bottom
    [STT-5]

--- content.js
+++ content.js
@@ -10,3 +10,16 @@ scrollToTopBottomBar.addEventListener("click", e => {
   window.scroll({ top: 0, behavior: "auto" });
 });
 document.body.appendChild(scrollToTopBottomBar);
+
+const scrollToBottomBottomBar = document.createElement("div");
+scrollToBottomBottomBar.className = "scroll-to-bottom-bottom-bar";
+scrollToBottomBottomBar.addEventListener("mouseenter", e => {
+  e.target.classList.add("increase-bottom-bar2");
+});
+scrollToBottomBottomBar.addEventListener("mouseleave", e => {
+  e.target.classList.remove("increase-bottom-bar2");
+});
+scrollToBottomBottomBar.addEventListener("click", e => {
+  window.scrollBy(0, window.innerHeight * 1000000);
+});
+document.body.appendChild(scrollToBottomBottomBar);
